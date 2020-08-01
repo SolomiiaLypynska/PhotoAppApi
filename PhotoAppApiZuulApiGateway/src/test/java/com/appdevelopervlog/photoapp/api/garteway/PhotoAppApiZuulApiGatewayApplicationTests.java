0fe4afa480b326e2b18f2884863f0870c90787c7
solya <yaremkosolomiya@gmail.com>
2020-08-01 22:13:43 +0300

    add zuul microservice

---
+++ PhotoAppApiZuulApiGateway/src/test/java/com/appdevelopervlog/photoapp/api/garteway/PhotoAppApiZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdevelopervlog.photoapp.api.garteway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
