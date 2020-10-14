13bacc72095fd1d13756ed7b1ed0f8f441c1bc0f
solya <yaremkosolomiya@gmail.com>
2020-10-14 20:29:06 +0300

    add responseentity status if user created

--- PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/ui/controllers/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/ui/controllers/UserController.java
@@ -6,6 +6,8 @@ import com.appdevelopervlog.photoapp.api.users.ui.model.CreateUserRequestModel;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,12 +25,11 @@ public class UserController {
     }
 
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails){
-        System.out.println("la " + userDetails.toString());
+    public ResponseEntity createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserDto userDto = modelMapper.map(userDetails, UserDto.class);
         usersService.createUser(userDto);
-        return "Create user";
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
