9ea96bc1f0638ba13e63df16a0d0fa709ef738f0
solya <yaremkosolomiya@gmail.com>
2020-12-06 00:48:19 +0200

    configure allow access to registration and login

--- PhotoAppApiZuulApiGateway/src/main/java/com/appdevelopervlog/photoapp/api/garteway/security/WebSecurity.java
+++ PhotoAppApiZuulApiGateway/src/main/java/com/appdevelopervlog/photoapp/api/garteway/security/WebSecurity.java
@@ -3,18 +3,17 @@ package com.appdevelopervlog.photoapp.api.garteway.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
-    private Environment environment;
+    private final Environment environment;
 
     @Autowired
     public WebSecurity(Environment environment) {
@@ -25,6 +24,11 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .antMatchers(environment.getProperty("api.h2console.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.registration.url.path")).permitAll()
+                .antMatchers(HttpMethod.POST, environment.getProperty("api.login.url.path")).permitAll()
+                .anyRequest().authenticated();
         // make API - STATELESS
         // no session will be created or used by Spring Security
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
