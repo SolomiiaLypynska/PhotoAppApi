000a0eb95b5b1c756c38a8b17e5a239c10f9e1e0
solya <yaremkosolomiya@gmail.com>
2020-12-05 17:26:14 +0200

    created LoginRequestModel model for login

---
+++ PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/ui/model/LoginRequestModel.java
@@ -0,0 +1,25 @@
+package com.appdevelopervlog.photoapp.api.users.ui.model;
+
+public class LoginRequestModel {
+    private String email;
+    private String password;
+
+    public LoginRequestModel() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
