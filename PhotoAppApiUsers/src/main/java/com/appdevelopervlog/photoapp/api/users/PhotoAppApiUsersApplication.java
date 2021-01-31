7b8dabe0137511b2a8f038b83b194f4eea7a5d43
solya <yaremkosolomiya@gmail.com>
2021-01-31 21:18:47 +0200

    add hystrix

--- PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -1,9 +1,9 @@
 package com.appdevelopervlog.photoapp.api.users;
 
-import com.appdevelopervlog.photoapp.api.users.shared.FeignErrorDecoder;
 import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -14,27 +14,28 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class PhotoAppApiUsersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+    }
 
-	@Bean
-	public BCryptPasswordEncoder passwordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
-	@Bean
-	Logger.Level feignLoggerLevel(){
-		return Logger.Level.FULL;
-	}
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 
 //	@Bean
 //	public FeignErrorDecoder getFeignErrorDecoder(){
--- PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -2,14 +2,25 @@ package com.appdevelopervlog.photoapp.api.users.data;
 
 import com.appdevelopervlog.photoapp.api.users.ui.model.AlbumResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "albums-ws")
+@FeignClient(name = "albums-ws", fallback = AlbusmsFallback.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albumsss")
-    public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+    List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbusmsFallback implements AlbumsServiceClient{
+
+    @Override
+    public List<AlbumResponseModel> getAlbums(String id) {
+        return new ArrayList<>();
+    }
 }
