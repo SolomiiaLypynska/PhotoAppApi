7823769c06f7d4bc62f6262be5bb5ed28e2dadd6
solya <yaremkosolomiya@gmail.com>
2021-03-04 22:40:53 +0200

    Error Handling with feign client Hystrix FallbackFactory

--- PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/data/AlbumsServiceClient.java
+++ PhotoAppApiUsers/src/main/java/com/appdevelopervlog/photoapp/api/users/data/AlbumsServiceClient.java
@@ -1,6 +1,11 @@
 package com.appdevelopervlog.photoapp.api.users.data;
 
+import com.appdevelopervlog.photoapp.api.users.service.UsersServiceImpl;
 import com.appdevelopervlog.photoapp.api.users.ui.model.AlbumResponseModel;
+import feign.FeignException;
+import feign.hystrix.FallbackFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name = "albums-ws", fallback = AlbusmsFallback.class)
+@FeignClient(name = "albums-ws", fallbackFactory = AlbusmsFallbackFactory.class)
 public interface AlbumsServiceClient {
 
     @GetMapping("/users/{id}/albums")
@@ -17,10 +22,31 @@ public interface AlbumsServiceClient {
 }
 
 @Component
-class AlbusmsFallback implements AlbumsServiceClient{
+class AlbusmsFallbackFactory implements FallbackFactory<AlbumsServiceClient> {
+
+
+    @Override
+    public AlbumsServiceClient create(Throwable throwable) {
+        return new AlbumsServiceClientFallback(throwable);
+    }
+}
+
+class AlbumsServiceClientFallback implements AlbumsServiceClient {
+    Logger logger = LoggerFactory.getLogger(AlbumsServiceClientFallback.class);
+
+    private final Throwable throwable;
+
+    public AlbumsServiceClientFallback(Throwable throwable) {
+        this.throwable = throwable;
+    }
 
     @Override
     public List<AlbumResponseModel> getAlbums(String id) {
+        if (throwable instanceof FeignException && ((FeignException) throwable).status() == 404){
+            logger.error("404 Error when userId {} . Error mesage - {}", id, throwable.getLocalizedMessage() );
+        } else {
+            logger.error("Other error took place: {}", throwable.getLocalizedMessage());
+        }
         return new ArrayList<>();
     }
 }
